148c2174d166f377bf45a628b9d9c1fff6595355
Harshvardhan Kadam <77354987+TheHarshKadam@users.noreply.github.com>
2021-07-03 15:06:24 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'dart:async';
+
+import 'package:bmi_calculator/pages/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  // ignore: non_constant_identifier_names
+  // ignore: unused_element
+  // ignore: non_constant_identifier_names
+  // ignore: unused_element
+  runApp(MyApp()) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: splash_screen(),
+    );
+  }
+}
+
+class splash_screen extends StatefulWidget {
+  @override
+  _splash_screenState createState() => _splash_screenState();
+}
+
+class _splash_screenState extends State<splash_screen> {
+  void initState() {
+    super.initState();
+    Timer(
+        Duration(seconds: 3),
+        () => Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => home())));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/pages/home.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class home extends StatefulWidget {
+  
+  @override
+  _homeState createState() => _homeState();
+}
+
+class _homeState extends State<home> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
