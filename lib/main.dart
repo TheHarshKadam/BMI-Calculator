f654d2994bad8c7426dc31a36ccf636d0c029c78
Harshvardhan Kadam <77354987+TheHarshKadam@users.noreply.github.com>
2021-07-05 12:28:35 +0530

    Splash Screen Added

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,14 @@ class splash_screen extends StatefulWidget {
 }
 
 class _splash_screenState extends State<splash_screen> {
- 
+  void initState() {
+    super.initState();
+    Timer(
+        Duration(seconds: 3),
+        () => Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => home())));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
