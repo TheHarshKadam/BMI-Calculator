8b40a2430967986eb0b4e29706e890bc55d67c63
Harshvardhan Kadam <77354987+TheHarshKadam@users.noreply.github.com>
2021-07-22 00:35:44 +0530

    Added ui widgets

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class home extends StatefulWidget {
-  
   @override
   _homeState createState() => _homeState();
 }
@@ -9,8 +8,75 @@ class home extends StatefulWidget {
 class _homeState extends State<home> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      backgroundColor: Color(0xFF130726),
+      appBar: AppBar(
+        backgroundColor: Color(0xFF130726),
+        elevation: 0.0,
+        centerTitle: true,
+        title: Text("BMI Calculator"),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                  margin: EdgeInsets.all(15.0),
+                  decoration: BoxDecoration(
+                    color: Color(0xFF1D1E3),
+                    borderRadius: BorderRadius.circular(10.0)
+                  ),
+                ),
+                ),
+                Expanded(
+                  child: Container(
+                  margin: EdgeInsets.all(15.0),
+                  decoration: BoxDecoration(
+                    color: Color(0xFF1D1E3),
+                    borderRadius: BorderRadius.circular(10.0)
+                  ),
+                ),
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+                  margin: EdgeInsets.all(15.0),
+                  decoration: BoxDecoration(
+                    color: Color(0xFF1D1E3),
+                    borderRadius: BorderRadius.circular(10.0)
+                  ),
+                ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                  margin: EdgeInsets.all(15.0),
+                  decoration: BoxDecoration(
+                    color: Color(0xFF1D1E3),
+                    borderRadius: BorderRadius.circular(10.0)
+                  ),
+                ),
+                ),
+                Expanded(
+                  child: Container(
+                  margin: EdgeInsets.all(15.0),
+                  decoration: BoxDecoration(
+                    color: Color(0xFF1D1E3),
+                    borderRadius: BorderRadius.circular(10.0)
+                  ),
+                ),
+                )
+              ],
+            ),
+          ),
+        ],
+      )
     );
   }
 }
