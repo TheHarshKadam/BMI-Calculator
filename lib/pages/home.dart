4ca5740fba7563e1ab77b79c92573d16cb975b34
Harshvardhan Kadam <77354987+TheHarshKadam@users.noreply.github.com>
2021-07-23 00:23:29 +0530

    icons added

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -9,74 +9,104 @@ class _homeState extends State<home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF130726),
-      appBar: AppBar(
         backgroundColor: Color(0xFF130726),
-        elevation: 0.0,
-        centerTitle: true,
-        title: Text("BMI Calculator"),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: Row(
-              children: [
-                Expanded(
-                  child: Container(
-                  margin: EdgeInsets.all(15.0),
-                  decoration: BoxDecoration(
-                    color: Color(0xFF1D1E3),
-                    borderRadius: BorderRadius.circular(10.0)
+        appBar: AppBar(
+          backgroundColor: Color(0xFF130726),
+          elevation: 0.0,
+          centerTitle: true,
+          title: Text("BMI Calculator"),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.all(15.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.male_rounded,
+                            size: 100.0,
+                            color: Colors.white,
+                          ),
+                          SizedBox(height: 10.0),
+                          Text(
+                            "MALE",
+                            style: TextStyle(
+                              fontSize: 20.0,
+                              color: Colors.white,
+                            ),
+                          )
+                        ],
+                      ),
+                      decoration: BoxDecoration(
+                          color: Color(0xFF1D1E3),
+                          borderRadius: BorderRadius.circular(10.0)),
+                    ),
                   ),
-                ),
-                ),
-                Expanded(
-                  child: Container(
-                  margin: EdgeInsets.all(15.0),
-                  decoration: BoxDecoration(
-                    color: Color(0xFF1D1E3),
-                    borderRadius: BorderRadius.circular(10.0)
-                  ),
-                ),
-                )
-              ],
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.all(15.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.female_rounded,
+                            size: 100.0,
+                            color: Colors.white,
+                          ),
+                          SizedBox(height: 10.0),
+                          Text(
+                            "FEMALE",
+                            style: TextStyle(
+                              fontSize: 20.0,
+                              color: Colors.white,
+                            ),
+                          )
+                        ],
+                      ),
+                      decoration: BoxDecoration(
+                          color: Color(0xFF1D1E3),
+                          borderRadius: BorderRadius.circular(10.0)),
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
-          Expanded(
-            child: Container(
-                  margin: EdgeInsets.all(15.0),
-                  decoration: BoxDecoration(
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
                     color: Color(0xFF1D1E3),
-                    borderRadius: BorderRadius.circular(10.0)
-                  ),
-                ),
-          ),
-          Expanded(
-            child: Row(
-              children: [
-                Expanded(
-                  child: Container(
-                  margin: EdgeInsets.all(15.0),
-                  decoration: BoxDecoration(
-                    color: Color(0xFF1D1E3),
-                    borderRadius: BorderRadius.circular(10.0)
-                  ),
-                ),
-                ),
-                Expanded(
-                  child: Container(
-                  margin: EdgeInsets.all(15.0),
-                  decoration: BoxDecoration(
-                    color: Color(0xFF1D1E3),
-                    borderRadius: BorderRadius.circular(10.0)
-                  ),
-                ),
-                )
-              ],
+                    borderRadius: BorderRadius.circular(10.0)),
+              ),
             ),
-          ),
-        ],
-      )
-    );
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.all(15.0),
+                      decoration: BoxDecoration(
+                          color: Color(0xFF1D1E3),
+                          borderRadius: BorderRadius.circular(10.0)),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.all(15.0),
+                      decoration: BoxDecoration(
+                          color: Color(0xFF1D1E3),
+                          borderRadius: BorderRadius.circular(10.0)),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ));
   }
 }
