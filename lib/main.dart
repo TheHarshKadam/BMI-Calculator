b3ef6cce738da31f3d69284b4470364bde14d437
Harshvardhan Kadam <77354987+TheHarshKadam@users.noreply.github.com>
2021-07-24 02:20:55 +0530

    Color Changes in Status bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:bmi_calculator/pages/home.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -18,15 +19,18 @@ class _splash_screenState extends State<splash_screen> {
   void initState() {
     super.initState();
     Timer(
-        Duration(seconds: 3),
+        Duration(seconds: 2),
         () => Navigator.pushReplacement(
             context, MaterialPageRoute(builder: (context) => home())));
   }
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Color(0xFF130726),
+    ));
     return Container(
-        color: Colors.deepPurple[900],
+        color: Color(0xFF130726),
         child: Center(
           child: Text("SPLASH SCREEN"),
         ));
