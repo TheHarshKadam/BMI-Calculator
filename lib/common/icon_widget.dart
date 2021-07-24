078f591b10c8ef13eedcb8f832a1380eb3ee19b4
Harshvardhan Kadam <77354987+TheHarshKadam@users.noreply.github.com>
2021-07-25 01:18:09 +0530

    created icon widget

---
+++ lib/common/icon_widget.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'card_widget.dart';
+
+const labelStyle = TextStyle(
+  fontSize: 18.0,
+  color: Color(0xFF8D8E98),
+);
+
+class iconWidget extends StatelessWidget {
+  const iconWidget({this.icon = Icons.male_rounded, this.label = "String"});
+  final IconData icon;
+  final String label;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          icon,
+          size: 80.0,
+          color: Colors.white,
+        ),
+        SizedBox(height: 15.0,),
+        Text(
+         "$label",
+          style: labelStyle,
+        )
+      ],
+    );
+  }
+}
