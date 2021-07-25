81daeb8ff9b14bdc251ac3d57b833d73e312f82a
Harshvardhan Kadam <77354987+TheHarshKadam@users.noreply.github.com>
2021-07-25 21:11:37 +0530

    functional changes

--- lib/common/card_widget.dart
+++ lib/common/card_widget.dart
@@ -3,12 +3,12 @@ import 'package:flutter/material.dart';
 class reusable_card extends StatelessWidget {
   reusable_card({
     this.colors = Colors.transparent, 
-    this.customiconWidget,
+    this.customchild,
     this.onPress,
   });
 
   final Color colors;
-  final Widget? customiconWidget;
+  final Widget? customchild;
   final void Function()? onPress;
 
   @override
@@ -16,13 +16,11 @@ class reusable_card extends StatelessWidget {
     return GestureDetector(
       onTap: onPress,
       child: Container(
-        child: customiconWidget,
+        child: customchild,
         margin: EdgeInsets.all(15.0),
         decoration: BoxDecoration(
-        color: colors, borderRadius: BorderRadius.circular(10.0)
-        ),
+            color: colors, borderRadius: BorderRadius.circular(10.0)),
       ),
     );
   }
 }
-
