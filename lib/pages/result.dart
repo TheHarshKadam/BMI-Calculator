955d5962f5caca6d7d914c12dd1145c649e321dd
Harshvardhan Kadam <77354987+TheHarshKadam@users.noreply.github.com>
2021-07-29 01:51:25 +0530

    created result page

--- lib/pages/result.dart
+++ lib/pages/result.dart
@@ -2,7 +2,6 @@ import 'package:bmi_calculator/common/icon_widget.dart';
 import 'package:flutter/material.dart';
 
 class ResultPage extends StatefulWidget {
-  const ResultPage({Key? key}) : super(key: key);
 
   @override
   _ResultPageState createState() => _ResultPageState();
@@ -12,12 +11,16 @@ class _ResultPageState extends State<ResultPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFF130726),
       appBar: AppBar(
-        title: Text(
-          "BMI Calculator",
-          style: TextStyle(
+         backgroundColor: Color(0xFF130726),
+         elevation: 0.0,
+          centerTitle: true,
+          title: Text(
+           "BMI Calculator",
+           style: TextStyle(
             color: Colors.white,
-          )
+            )
           ),
       ),
     );
