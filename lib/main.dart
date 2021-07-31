ea79fa8f9080286eeb4fb1a0f4482977d5ebf751
Harshvardhan Kadam <77354987+TheHarshKadam@users.noreply.github.com>
2021-08-01 00:51:15 +0530

    Added responsiveness for different screen sizes

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,40 @@
 //--no-sound-null-safety
 import 'dart:async';
-
 import 'package:bmi_calculator/pages/home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:bmi_calculator/pages/result.dart';
+import 'package:responsive_framework/responsive_framework.dart';
+import 'package:sizer/sizer.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: splash_screen(),
-  ));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Sizer(
+      builder: (context, orientation, deviceType) {
+        return MaterialApp(
+          builder: (context, widget) => ResponsiveWrapper.builder(
+              BouncingScrollWrapper.builder(context, widget!),
+              maxWidth: 1200,
+              minWidth: 450,
+              defaultScale: true,
+              breakpoints: [
+                ResponsiveBreakpoint.resize(450, name: MOBILE),
+                ResponsiveBreakpoint.autoScale(800, name: TABLET),
+                ResponsiveBreakpoint.autoScale(1000, name: TABLET),
+                ResponsiveBreakpoint.autoScale(1200, name: DESKTOP),
+                ResponsiveBreakpoint.autoScale(2460, name: "4K"),
+              ]),
+          debugShowCheckedModeBanner: false,
+          home: splash_screen(),
+        );
+      },
+    );
+  }
 }
 
 class splash_screen extends StatefulWidget {
