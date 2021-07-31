3db3f559e6d21fe19363495d21ead1f5082fcb32
Harshvardhan Kadam <77354987+TheHarshKadam@users.noreply.github.com>
2021-07-31 13:27:30 +0530

    Functional and Constructor Changes

--- lib/pages/result.dart
+++ lib/pages/result.dart
@@ -2,10 +2,11 @@ import 'package:bmi_calculator/common/icon_widget.dart';
 import 'package:bmi_calculator/pages/home.dart';
 import 'package:flutter/material.dart';
 import 'package:bmi_calculator/common/card_widget.dart';
+import 'package:flutter/services.dart';
 
 class ResultPage extends StatelessWidget {
-  
-  ResultPage({ required this.bmiResult, required this.finalresult,required this.conclusion});
+  ResultPage(
+      {this.bmiResult = '', this.finalresult = '', this.conclusion = ''});
 
   final String bmiResult;
   final String finalresult;
@@ -49,7 +50,7 @@ class ResultPage extends StatelessWidget {
                   Expanded(
                     child: Center(
                       child: Text(
-                        bmiResult,
+                        finalresult,
                         style: TextStyle(
                           color: Color(0xFF24D876),
                           fontSize: 22.0,
@@ -60,7 +61,7 @@ class ResultPage extends StatelessWidget {
                   ),
                   Expanded(
                     child: Text(
-                      finalresult,
+                      bmiResult,
                       style: TextStyle(
                           fontSize: 100.0,
                           fontWeight: FontWeight.bold,
@@ -68,10 +69,16 @@ class ResultPage extends StatelessWidget {
                     ),
                   ),
                   Expanded(
-                    child: Text(
-                      conclusion,
-                      style: TextStyle(fontSize: 18.0, color: Colors.white),
-                      textAlign: TextAlign.center,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        conclusion,
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          color: Colors.white,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
                     ),
                   ),
                 ],
@@ -80,18 +87,24 @@ class ResultPage extends StatelessWidget {
           ),
           GestureDetector(
             onTap: () {
+              Feedback.forTap(context);
+              Clipboard.setData(ClipboardData());
+              HapticFeedback.lightImpact();
               Navigator.pop(context);
             },
             child: Container(
               child: Center(
                 child: Text("RE-CALCULATE!",
-                    style: TextStyle(color: Colors.black87, fontSize: 30.0)),
+                    style: TextStyle(
+                        color: Colors.black87,
+                        fontSize: 30.0,
+                        fontWeight: FontWeight.bold)),
               ),
               margin: EdgeInsets.only(top: 10.0),
               height: 80.0,
               width: double.infinity,
               decoration: BoxDecoration(
-                  color: Color(0xFFEB1555),
+                  color: Color(0xFFEE4266),
                   borderRadius:
                       BorderRadius.vertical(top: Radius.circular(18.0))),
             ),
